09da6afe1bd69803c6d2d375227a3085d1bd05df
Galvanize <Galvanize@Alexs-MacBook-Pro.local>
2015-10-10 14:18:23 -0600

    Added a bunch of media and interactivity

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+	$(document).ready(function(){
+		$(".naeNae").hide();
+	});
+
 	$("#day").click(function(){
 		$(".mainBg").toggleClass("nightBg", 1500);
 		$("p").toggleClass("nightFont", 1500);
@@ -8,6 +12,37 @@
 		$("#day").text($(this).text() == "night" ? "day" : "night");
 		$("#day").toggleClass("nightButton");
 		$(".linkin").toggleClass("nightLink", 1500);
-	});
+	})
+
+      $(document).ready(function() {
+        var audioElement = document.createElement('audio');
+        audioElement.setAttribute('src', 'media/watchMe.mp3');
+        //audioElement.load()
+        $.get();
+        audioElement.addEventListener("load", function() {
+        audioElement.play();
+        }, true);
+
+        $('#feelings').click(function() {
+        		audioElement.play();
+        });
+        $('.pause').click(function() {
+        		audioElement.pause();
+        });
+        $(".play").click(function(){
+        	    audioElement.play();
+        })
+        $("#hideDis").click(function(){
+      			audioElement.pause();
+    })
+})
+    $("#feelings").click(function(){
+		$(".naeNae").show(900);
+		})
+  	$("#hideDis").click(function(){
+      	$(".naeNae").hide(900);
+    })
+
+
 
 
